4ad118e9497fbf0f3043a4c0a5dc9a75a1423165
Vaibhav Kaul <spartanpro1231@gmail.com>
2020-08-07 19:42:35 +0530

    Add files via upload
    UI Updated with dark theme

--- app.js
+++ app.js
@@ -1,43 +1,37 @@
-
 //Here we select the input whose value will be the name of new create list whithin list container.
-var addItemBtn=document.querySelector('input[type="submit"]');
+var addItemBtn = document.querySelector('input[type="submit"]');
 
 //A state variable which keeps track for the editable content of the list
-let editable=true;
+let editable = true;
 
 //Here we select the add button that adds the the value of above input to list container.
-var inputItemName=document.querySelector('.inputName');
+var inputItemName = document.querySelector(".inputName");
 
 //Trigger an click event when we click the add buttton and call addListItem() function.
-addItemBtn.addEventListener('click',addListItem);
+addItemBtn.addEventListener("click", addListItem);
 
 //Here we select the list container.
-const listContainer=document.getElementById('listContainer');
+const listContainer = document.getElementById("listContainer");
 
 //Add an event listener on the list container.
-listContainer.addEventListener('click',deleteListItem);
+listContainer.addEventListener("click", deleteListItem);
 
-    //Function is called when add button is clicked.
-    function addListItem(ev)
-    {
-        //This prevents the data of list from deleting when the function finishes its work.
-        ev.preventDefault();
-        
-        if( inputItemName.value=='' || !/[a-zA-Z0-9]+/.test(inputItemName.value))
-        {
-            console.log(inputItemName.value);
-            //Display an error message on the webpage.
-            document.getElementById('errorMessage').innerHTML='<h4>Please Enter Some Value In The Text-Box.</h4>';
-        }
-        else
-        {
-            
-            //Remove the error message from the page.
-            document.getElementById('errorMessage').innerHTML='';
+//Function is called when add button is clicked.
+function addListItem(ev) {
+	//This prevents the data of list from deleting when the function finishes its work.
+	ev.preventDefault();
 
-            //Add the list item in the list container here by using innerHTML property.
-            listContainer.innerHTML+=
-            `
+	if (inputItemName.value == "" || !/[a-zA-Z0-9]+/.test(inputItemName.value)) {
+		console.log(inputItemName.value);
+		//Display an error message on the webpage.
+		inputItemName.classList.add("errorMessage");
+	} else {
+		inputItemName.classList.remove("errorMessage");
+		//Remove the error message from the page.
+		//document.getElementById("errorMessage").innerHTML = "";
+
+		//Add the list item in the list container here by using innerHTML property.
+		listContainer.innerHTML += `
             <li class='listItem'>
                 <p class='listContent'>${inputItemName.value}</p>
                  <button class='closeBtn'> X</button>
@@ -47,74 +41,71 @@ listContainer.addEventListener('click',deleteListItem);
              </li>
             `;
 
-            //Add the list container into local storage by stringifying it.
-            localStorage.setItem('localListItems',JSON.stringify(listContainer.innerHTML));
+		//Add the list container into local storage by stringifying it.
+		localStorage.setItem(
+			"localListItems",
+			JSON.stringify(listContainer.innerHTML)
+		);
 
-            //Once a list item is added clear/reset the input.
-            inputItemName.value='';
-        }
-        
+		//Once a list item is added clear/reset the input.
+		inputItemName.value = "";
+	}
 }
 
-    //Function gets called when the delete cross button is clicked on a list item.
-    function deleteListItem(e)
-    {
-        e.preventDefault();
+//Function gets called when the delete cross button is clicked on a list item.
+function deleteListItem(e) {
+	e.preventDefault();
 
-        //Check which child of list container contains class=closeBtn and point to that child.
-        if(e.target.classList.contains('closeBtn'))
-        {
-            if(confirm('Are You Sure About That??'))
-            {
-                //Once the child having closeBtn class is found then removes its parent from the DOM.
-                e.target.parentNode.remove();
-            }
+	//Check which child of list container contains class=closeBtn and point to that child.
+	if (e.target.classList.contains("closeBtn")) {
+		if (confirm("Are You Sure About That??")) {
+			//Once the child having closeBtn class is found then removes its parent from the DOM.
+			e.target.parentNode.remove();
+		}
 
-            //Update the list container to local storage.
-            localStorage.setItem('localListItems',JSON.stringify(listContainer.innerHTML));
-        }
-        
-        
-    }
+		//Update the list container to local storage.
+		localStorage.setItem(
+			"localListItems",
+			JSON.stringify(listContainer.innerHTML)
+		);
+	}
+}
 
-    function editTask(e){
-        if(editable){
-            e.parentNode.children[0].setAttribute('contenteditable','true');
-            console.log(e.children[0].src);
-            e.children[0].src='done.png';
-            editable=false;
-        }
-        else{
-            e.parentNode.children[0].setAttribute('contenteditable','false');
-            e.children[0].src='pencil.png';
-            editable=true;
-            if(e.parentNode.children[0].textContent.length>0){
-                localStorage.setItem('localListItems',JSON.stringify(listContainer.innerHTML));
-            }
-            else{
-                alert('Since You Have Not Entered Anything \n So The Previous Value Will Be Displayed on Page Reload.');
-            }
-           
-        }
-    }
-    
-   //WHen the body loads then fetch the list items from the local storage and append them in the list container.
-   const fetchDataFromLocalStorage=()=>{
-       listContainer.innerHTML=JSON.parse(localStorage.getItem('localListItems'));
-       let listItemName=document.getElementsByTagName('li');
-        console.log("The number of list items are : "+listItemName.length);
-       inputItemName.focus();
-    };
+function editTask(e) {
+	if (editable) {
+		e.parentNode.children[0].setAttribute("contenteditable", "true");
+		console.log(e.children[0].src);
+		e.children[0].src = "done.png";
+		editable = false;
+	} else {
+		e.parentNode.children[0].setAttribute("contenteditable", "false");
+		e.children[0].src = "pencil.png";
+		editable = true;
+		if (e.parentNode.children[0].textContent.length > 0) {
+			localStorage.setItem(
+				"localListItems",
+				JSON.stringify(listContainer.innerHTML)
+			);
+		} else {
+			alert(
+				"Since You Have Not Entered Anything \n So The Previous Value Will Be Displayed on Page Reload."
+			);
+		}
+	}
+}
 
-   
-    //When window loads then the list box appears
-    window.addEventListener('load',()=>{
-        setTimeout(fetchDataFromLocalStorage,1500);
-    });
+//WHen the body loads then fetch the list items from the local storage and append them in the list container.
+const fetchDataFromLocalStorage = () => {
+	listContainer.innerHTML = JSON.parse(localStorage.getItem("localListItems"));
+	let listItemName = document.getElementsByTagName("li");
+	console.log("The number of list items are : " + listItemName.length);
+	inputItemName.focus();
+};
 
-    window.addEventListener('DOMContentLoaded',()=>{
-        document.getElementById('mainList').style.cssText='transform:translateY(0px);';
-        document.querySelector('.addItemHeading').style.cssText='transform:translateX(0px);';
-    });
+//When window loads then the list box appears
 
-    
+window.addEventListener("DOMContentLoaded", () => {
+	document.getElementById("mainList").style.cssText = "opacity:1;";
+	fetchDataFromLocalStorage();
+	listContainer.style.cssText = "opacity:1;";
+});
--- service-worker.js
+++ service-worker.js
@@ -1,54 +1,52 @@
-const staticCache = "list-v4";
+const staticCache = "list-v23 ";
 const assets = [
-  "./",  
-  "./index.html",
-  "./app.js",
-  "./style.css",
-  "./app-icon.png",
-  "./bg-3.gif",
-  "./icon-72x72.png",
-  "./icon-96x96.png",
-  "./icon-128x128.png",
-  "./icon-144x144.png",
-  "./app-icon-144x144.png",
-  "./icon-152x152.png",
-  "./icon-192x192.png",
-  "./icon-384x384.png",
-  "./icon-512x512.png",
-  "./pencil.png",
-  "./done.png"
-  
+	"./",
+	"./index.html",
+	"./app.js",
+	"./style.css",
+	"./app-icon.png",
+	"./bg-3.gif",
+	"./icon-72x72.png",
+	"./icon-96x96.png",
+	"./icon-128x128.png",
+	"./icon-144x144.png",
+	"./app-icon-144x144.png",
+	"./icon-152x152.png",
+	"./icon-192x192.png",
+	"./icon-384x384.png",
+	"./icon-512x512.png",
+	"./pencil.png",
+	"./done.png",
 ];
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticCache).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-  //Activates on its own
-  self.skipWaiting();
+self.addEventListener("install", (installEvent) => {
+	installEvent.waitUntil(
+		caches.open(staticCache).then((cache) => {
+			cache.addAll(assets);
+		})
+	);
+	//Activates on its own
+	self.skipWaiting();
 });
 
-self.addEventListener('activate',evt=>{
-  evt.waitUntil(
-    caches.keys()
-    .then(arrayRes=>{
-     let removeArrayElement= arrayRes.filter(element=>element.includes('list'));
-     removeArrayElement.map(element=>{
-       if(element!==staticCache){
-         caches.delete(element);
-       }
-     })
- 
-    })
-  );
+self.addEventListener("activate", (evt) => {
+	evt.waitUntil(
+		caches.keys().then((arrayRes) => {
+			let removeArrayElement = arrayRes.filter((element) =>
+				element.includes("list")
+			);
+			removeArrayElement.map((element) => {
+				if (element !== staticCache) {
+					caches.delete(element);
+				}
+			});
+		})
+	);
 });
 
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-      caches.match(fetchEvent.request)
-      .then(res => {
-        return res || fetch(fetchEvent.request)
-      })
-    );
-  });
+self.addEventListener("fetch", (fetchEvent) => {
+	fetchEvent.respondWith(
+		caches.match(fetchEvent.request).then((res) => {
+			return res || fetch(fetchEvent.request);
+		})
+	);
+});
