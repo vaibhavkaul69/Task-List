ee7a6f2afdeff1c9f8c5044462ef309e4c1ed851
unknown <vaibhav.kaul2@gnmail.com>
2020-08-13 21:15:34 +0530

    Input UI color changed

--- service-worker.js
+++ service-worker.js
@@ -1,52 +1,54 @@
-const staticCache = "list-v24 ";
+/** @format */
+
+const staticCache = 'list-v25 ';
 const assets = [
-	"./",
-	"./index.html",
-	"./app.js",
-	"./style.css",
-	"./app-icon.png",
-	"./bg-3.gif",
-	"./icon-72x72.png",
-	"./icon-96x96.png",
-	"./icon-128x128.png",
-	"./icon-144x144.png",
-	"./app-icon-144x144.png",
-	"./icon-152x152.png",
-	"./icon-192x192.png",
-	"./icon-384x384.png",
-	"./icon-512x512.png",
-	"./pencil.png",
-	"./done.png",
+	'./',
+	'./index.html',
+	'./app.js',
+	'./style.css',
+	'./app-icon.png',
+	'./bg-3.gif',
+	'./icon-72x72.png',
+	'./icon-96x96.png',
+	'./icon-128x128.png',
+	'./icon-144x144.png',
+	'./app-icon-144x144.png',
+	'./icon-152x152.png',
+	'./icon-192x192.png',
+	'./icon-384x384.png',
+	'./icon-512x512.png',
+	'./pencil.png',
+	'./done.png',
 ];
-self.addEventListener("install", (installEvent) => {
+self.addEventListener('install', (installEvent) => {
 	installEvent.waitUntil(
 		caches.open(staticCache).then((cache) => {
 			cache.addAll(assets);
-		})
+		}),
 	);
 	//Activates on its own
 	self.skipWaiting();
 });
 
-self.addEventListener("activate", (evt) => {
+self.addEventListener('activate', (evt) => {
 	evt.waitUntil(
 		caches.keys().then((arrayRes) => {
 			let removeArrayElement = arrayRes.filter((element) =>
-				element.includes("list")
+				element.includes('list'),
 			);
 			removeArrayElement.map((element) => {
 				if (element !== staticCache) {
 					caches.delete(element);
 				}
 			});
-		})
+		}),
 	);
 });
 
-self.addEventListener("fetch", (fetchEvent) => {
+self.addEventListener('fetch', (fetchEvent) => {
 	fetchEvent.respondWith(
 		caches.match(fetchEvent.request).then((res) => {
 			return res || fetch(fetchEvent.request);
-		})
+		}),
 	);
 });
